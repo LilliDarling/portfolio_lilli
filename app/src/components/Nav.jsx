618dd275b94d6e589a0ae52c8f17f805c478f482
Lillith Long <valkyrieremedy@gmail.com>
2024-09-16 16:11:11 -0600

    Nav update

--- app/src/components/Nav.jsx
+++ app/src/components/Nav.jsx
@@ -8,12 +8,12 @@ export default function SideNav() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse>
           <Nav>
-            <Nav.Link>Home</Nav.Link>
-            <Nav.Link>About</Nav.Link>
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/about">About</Nav.Link>
             <Nav.Link>Projects</Nav.Link>
-            <Nav.Link>Connect</Nav.Link>
-            <Nav.Link>GitHub</Nav.Link>
-            <Nav.Link>LinkedIn</Nav.Link>
+            <Nav.Link href="/connect">Connect</Nav.Link>
+            <Nav.Link href="https://github.com/LilliDarling">GitHub</Nav.Link>
+            <Nav.Link href="https://www.linkedin.com/in/lillith-long/">LinkedIn</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
