9d96cd455573eaf3aa6aaf2a15caf585d01cfa4f
Lillith Long <valkyrieremedy@gmail.com>
2024-09-16 15:58:11 -0600

    Connect component

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from './components/Home.jsx';
 import SideNav from './components/Nav.jsx'
 import About from "./components/About.jsx";
+import Connect from "./components/contact/Connect.jsx";
 
 export default function App(){
   return (
@@ -10,6 +11,7 @@ export default function App(){
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/connect" element={<Connect />} />
       </Routes>
     </BrowserRouter>
   )
--- app/src/components/contact/Connect.jsx
+++ app/src/components/contact/Connect.jsx
@@ -1,8 +1,22 @@
+import Contact from "./ContactForm"
+
 export default function Connect() {
   return (
     <>
       <div>
-        
+        <Contact />
+      </div>
+      <div>
+        <h1>Connect with Me!</h1>
+        <p>If you would like to work with me or just shoot ideas feel free to send me a message here or link up with me through one of the links below! I am always looking for new collabs and innovative solutions in software engineering.</p>
+        <div>
+          <ul>
+            <li>Discord</li>
+            <li>Telegram</li>
+            <li>LinkedIn</li>
+            <li>GitHub</li>
+          </ul>
+        </div>
       </div>
     </>
   )
