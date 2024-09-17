65e96bf642c8f38f47c82a158200fb7b22307999
Lillith Long <valkyrieremedy@gmail.com>
2024-09-17 16:26:56 -0600

    Projects file for components

---
+++ app/src/components/projects/projectsFile.js
@@ -0,0 +1,18 @@
+export const projects = {
+  1: {
+    id: 1,
+    title: "Witchy Cooking",
+    description: "As part of an intensive software development program, I not only mastered the core curriculum but significantly expanded upon it, tailoring the knowledge to my unique vision. I enhanced the project by implementing dynamic form functionality and robust security measures. Going above and beyond the course requirements, I independently learned and integrated advanced deployment techniques, showcasing my ability to self-teach and push the boundaries of my skills.",
+    stack: ["Django", "Python", "JavaScript", "PostgreSQL"],
+    repo: "https://gitlab.com/LilliDarling/witchycooking",
+    link: "https://witchycooking.com"
+  },
+  2: {
+    id: 2,
+    title: "Growing Me",
+    description: "Working on this",
+    stack: ["FastAPI", "Python", "JavaScript", "React", "MongoDB"],
+    repo: "https://github.com/LilliDarling/GrowingMe",
+    link: "https://www.growingme.com"
+  }
+}
