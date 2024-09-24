64227aa6244d669873f0e4253a682ee4045e9302
Lillith Long <valkyrieremedy@gmail.com>
2024-09-24 10:43:30 -0600

    Connect Component Updated

--- app/src/components/contact/Connect.jsx
+++ app/src/components/contact/Connect.jsx
@@ -1,4 +1,5 @@
 import Contact from "./ContactForm"
+import { ListGroup } from "react-bootstrap"
 
 export default function Connect() {
   return (
@@ -10,12 +11,12 @@ export default function Connect() {
         <h1>Connect with Me!</h1>
         <p>If you would like to work with me or just shoot ideas feel free to send me a message here or link up with me through one of the links below! I am always looking for new collabs and innovative solutions in software engineering.</p>
         <div>
-          <ul>
-            <li>Discord</li>
-            <li>Telegram</li>
-            <li>LinkedIn</li>
-            <li>GitHub</li>
-          </ul>
+          <ListGroup>
+            <ListGroup.Item>Discord</ListGroup.Item>
+            <ListGroup.Item>Telegram</ListGroup.Item>
+            <ListGroup.Item>LinkedIn</ListGroup.Item>
+            <ListGroup.Item>GitHub</ListGroup.Item>
+          </ListGroup>
         </div>
       </div>
     </>
