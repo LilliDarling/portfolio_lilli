210114088273b72f4dee1da588ffe36ca4f9f594
Lillith Long <valkyrieremedy@gmail.com>
2024-09-25 11:25:38 -0600

    Navbar CSS adjusted

--- app/src/components/Nav.jsx
+++ app/src/components/Nav.jsx
@@ -1,10 +1,19 @@
 import { Nav, Navbar, Container, Image } from 'react-bootstrap'
-import LogoImage from '../assets/icons/logo.png'
+import { useMediaQuery } from 'react-responsive'
 import '../style/sidenav.css'
 import { useState } from 'react'
 
+import LogoImage from '../assets/icons/logo.png'
+import homeImage from '../assets/icons/home.png'
+import aboutImage from '../assets/icons/about.png'
+import connectImage from '../assets/icons/connect.png'
+import projectsImage from '../assets/icons/projects.png'
+import githubImage from '../assets/icons/github.png'
+import linkedinImage from '../assets/icons/linkedin.png'
+
 export default function SideNav() {
   const [expanded, setExpanded] = useState(false)
+  const isDesktop = useMediaQuery({ minWidth: 992 })
 
   const toggleExpanded = () => {
     setExpanded(!expanded)
@@ -15,23 +24,51 @@ export default function SideNav() {
   }
 
   return (
-    <Navbar expanded={expanded} className={`sidebar-nav flex-column ${expanded ? 'expanded' : ''}`} expand="lg">
+    <Navbar 
+      expanded={expanded} 
+      className={`sidebar-nav ${expanded ? 'expanded' : ''} ${isDesktop ? 'desktop' : 'mobile'}`} 
+      expand={isDesktop ? true : "lg"}>
       <Container fluid className="d-flex flex-column h-100 p-0">
-        <Navbar.Brand>
-          <Image src={LogoImage} alt="Lillith Long" />
-        </Navbar.Brand>
-        <Navbar.Toggle
-          aria-controls="basic-navbar-nav"
-          onClick={toggleExpanded}
-        />
+        <div className="nav-header d-flex justify-content-between align-items-center w-100 p-3">
+          <Navbar.Brand>
+            <Image src={LogoImage} alt="Lillith Long" className="logo ms-2" />
+          </Navbar.Brand>
+          <Navbar.Toggle
+            aria-controls="basic-navbar-nav"
+            onClick={toggleExpanded}
+            className="me-3"
+          />
+        </div>
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="flex-column">
-            <Nav.Link href="/" onClick={closeNav}>Home</Nav.Link>
-            <Nav.Link href="/about" onClick={closeNav}>About</Nav.Link>
-            <Nav.Link href="projects" onClick={closeNav}>Projects</Nav.Link>
-            <Nav.Link href="/connect" onClick={closeNav}>Connect</Nav.Link>
-            <Nav.Link href="https://github.com/LilliDarling" onClick={closeNav}>GitHub</Nav.Link>
-            <Nav.Link href="https://www.linkedin.com/in/lillith-long/" onClick={closeNav}>LinkedIn</Nav.Link>
+            <div className="nav-links mb-5">
+              <Nav.Link href="/" onClick={closeNav} className="nav-item mb-3">
+                <Image src={homeImage} alt="Home" />
+                <span className="nav-label">Home</span>
+              </Nav.Link>
+              <Nav.Link href="/about" onClick={closeNav} className="nav-item mb-3">
+                <Image src={aboutImage} alt="About" />
+                <span className="nav-label">About</span>
+              </Nav.Link>
+              <Nav.Link href="/projects" onClick={closeNav} className="nav-item mb-3">
+                <Image src={projectsImage} alt="Projects" />
+                <span className="nav-label">Projects</span>
+              </Nav.Link>
+              <Nav.Link href="/connect" onClick={closeNav} className="nav-item mb-3">
+                <Image src={connectImage} alt="Connect" />
+                <span className="nav-label">Connect</span>
+              </Nav.Link>
+            </div>
+            <div className="social-links">
+              <Nav.Link href="https://github.com/LilliDarling" onClick={closeNav} target="_blank" className="nav-item mb-3">
+                <Image src={githubImage} alt="GitHub" />
+                <span className="nav-label">GitHub</span>
+              </Nav.Link>
+              <Nav.Link href="https://www.linkedin.com/in/lillith-long/" onClick={closeNav} target="_blank" className="nav-item">
+                <Image src={linkedinImage} alt="LinkedIn" />
+                <span className="nav-label">LinkedIn</span>
+              </Nav.Link>
+            </div>
           </Nav>
         </Navbar.Collapse>
       </Container>
