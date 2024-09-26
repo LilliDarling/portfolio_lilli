4e087623a099c1b11cc080d2c89c96a3cc4af3d4
Lillith Long <valkyrieremedy@gmail.com>
2024-09-26 12:17:57 -0600

    Contact comp adjusted

--- app/src/components/contact/Connect.jsx
+++ app/src/components/contact/Connect.jsx
@@ -1,5 +1,10 @@
 import Contact from "./ContactForm"
-import { ListGroup } from "react-bootstrap"
+import { ListGroup, Image } from "react-bootstrap"
+
+import discordImage from "../../assets/icons/discord.png"
+import telegramImage from "../../assets/icons/telegram.png"
+import linkedinImage from "../../assets/icons/linkedin.png"
+import githubImage from "../../assets/icons/github.png"
 
 export default function Connect() {
   return (
@@ -12,10 +17,18 @@ export default function Connect() {
         <p>If you would like to work with me or just shoot ideas feel free to send me a message here or link up with me through one of the links below! I am always looking for new collabs and innovative solutions in software engineering.</p>
         <div>
           <ListGroup>
-            <ListGroup.Item>Discord</ListGroup.Item>
-            <ListGroup.Item>Telegram</ListGroup.Item>
-            <ListGroup.Item>LinkedIn</ListGroup.Item>
-            <ListGroup.Item>GitHub</ListGroup.Item>
+            <ListGroup.Item action href="https://discordapp.com/users/814526671549235209" target="_blank">
+              <Image src={discordImage} alt="Discord" />
+            </ListGroup.Item>
+            <ListGroup.Item action href="https://t.me/@Lilli_Darling" target="_blank">
+              <Image src={telegramImage} alt="Telegram" />
+            </ListGroup.Item>
+            <ListGroup.Item action href="https://www.linkedin.com/in/lillith-long/" target="_blank">
+              <Image src={linkedinImage} alt="LinkedIn" />
+            </ListGroup.Item>
+            <ListGroup.Item action href="https://github.com/LilliDarling" target="_blank">
+              <Image src={githubImage} alt="GitHub" />
+            </ListGroup.Item>
           </ListGroup>
         </div>
       </div>
