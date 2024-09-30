d23fd3bb1542a009ff3adaff0264d4a7d935e78f
Lillith Long <valkyrieremedy@gmail.com>
2024-09-30 13:09:49 -0600

    Projects gallery CSS adjusted

--- app/src/components/projects/Gallery.jsx
+++ app/src/components/projects/Gallery.jsx
@@ -1,14 +1,16 @@
-import { Image } from 'react-bootstrap'
+import { Image, Container, Row, Col } from 'react-bootstrap'
+import "../../style/projects.css"
 
 export default function ProjectGallery({ project }) {
   return (
-    <div>
-      {project.images.map((image, index) =>(
-        <Image
-          key={index}
-          src={image}
-        />
-      ))}
-    </div>
+    <Container>
+      <Row className="gallery-row">
+        {project.images.map((image, index) =>(
+          <Col key={index} xs={12} md={6} lg={3} className="gallery-col">
+            <Image src={image} fluid className="gallery-image" />
+          </Col>
+        ))}
+      </Row>
+    </Container>
   )
 }
